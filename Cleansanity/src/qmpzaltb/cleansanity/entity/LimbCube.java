05a0234a84cd1d0d13cfbbf2ea7cc9c991075619
qmpzaltb <mark.b.ftw@gmail.com>
2013-11-23 00:24:10 -0500

    A class that may help with VBOzuliation. Or display lists. Eh, better read up on what is faster.

---
+++ Cleansanity/src/qmpzaltb/cleansanity/entity/LimbCube.java
@@ -0,0 +1,53 @@
+package qmpzaltb.cleansanity.entity;
+
+public class LimbCube extends Limb{
+
+	@Override
+	public float getAttachX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getAttachY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float getAttachHeading() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLimbTo(float... limbCoords) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLimbSize(float size) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setLimbHeading(float heading) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setColour(float r, float g, float b, float a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawLimb(float x, float y, float z, float heading) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
